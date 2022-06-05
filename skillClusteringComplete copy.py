d41ba1b7b949a5947a78c1613ff7ffec89232131
milinthalekh1912 <milinthalekh1912@gmail.com>
2022-06-05 19:41:43 +0700

    commit

--- skillClusteringComplete copy.py
+++ skillClusteringComplete copy.py
@@ -1,23 +1,15 @@
-from cProfile import label
-from email.policy import default
-from traceback import print_tb
-from unittest import result
-from matplotlib import pyplot as plt
 import pandas as pd
-from pygments import highlight
-from sklearn.preprocessing import LabelEncoder
-from sklearn.decomposition import PCA
-import numpy as np
 from pyexpat import model
 from sklearn.cluster import KMeans
 
 df = pd.read_excel("DatabaseOK.xlsx",sheet_name="Skill")
 
+
 x = df.drop("Y",axis=1)
 req = "CE"
 
 model = KMeans(n_clusters=5)
-y_Kmeans = model.fit_predict(x)
+y_Kmeans = model.fit_predict(x.values)
 
 z = x
 z["Y"] = y_Kmeans
@@ -25,17 +17,20 @@ z['Class'] = ["CS", "CE", "SE", "IT", "BC"]
 
 print(z[['Y','Class']])
 
-data_Test = [0,0,0,0,0,0]
+data_Test = [1,1,1,0,0,0]
 
 # result = model.predict([data_Test])
-print(model.predict([data_Test]))
-
+predicValue = model.predict([data_Test])[0]
+print()
+v = z["Class"].loc[z['Y']== predicValue]
+print(v.values[0])
+print()
 rowRequest = df.loc[x["Class"] == req]
 columnRequest = rowRequest.columns.values
 classRequest = rowRequest.values.tolist()[0]
 
 resultRequest = []
-
+print()
 for i in range(0,len(data_Test)):
     resultRequest.append(float(data_Test[i]) - float(classRequest[i]))
 
@@ -45,28 +40,4 @@ for i in range(0,len(columnRequest)-1):
     else:
         print(columnRequest[i] + " PASS")
 
-# #นำ Columns เข้าวิชาที่ต้องเรียน
-# rowRequest = df.loc[x["Class"] == req]
-
-# rowRequest = rowRequest.drop(["Y"], axis=1)
-# columnRequest = rowRequest.columns.values
-# classRequest = rowRequest.values.tolist()[0]
-
-# resultRequest = []
-
-# #วิชาที่ต้องเรียน
-# print(classRequest)
-
-# #วิชาที่รับเข้า
-# print(data_Test)
-
-# #ค้นหาวิชาที่ต้องเรียนเพิ่ม
-# for i in range(0,len(data_Test)):
-#     resultRequest.append(float(data_Test[i]) - float(classRequest[i]))
-
-# for i in range(0,len(columnRequest)):
-#     if(resultRequest[i] < 0):
-#         print(columnRequest[i] + " Not PASS")
-#     else:
-#         print(columnRequest[i] + " PASS")
 
